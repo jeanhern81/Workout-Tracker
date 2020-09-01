fe8c8f2e10ed3db196506eac67a1f22b848ca639
jeanhern81 <jeanhern81@gmail.com>
2020-09-01 15:52:51 -0700

    added commented out on sever and created model folder

--- server.js
+++ server.js
@@ -5,13 +5,16 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+
+
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-//require("./routes/apiRoutes");
-//require("./routes/htmlRoutes");
+
+//require("./routes/apiRoutes")(app);
+//require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function () {
     console.log(`App running on port ${PORT}!`);
