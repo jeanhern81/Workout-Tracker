63f485e2eb5c09ddd3a53cbf62c0949c693f6059
jeanhern81 <jeanhern81@gmail.com>
2020-09-01 15:10:12 -0700

    created server and it's working with commented out area for routes

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+//require("./routes/apiRoutes");
+//require("./routes/htmlRoutes");
+
+app.listen(PORT, function () {
+    console.log(`App running on port ${PORT}!`);
+  
+});
