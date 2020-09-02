781ced8399dcda0e66f12df83218ac7c02acbc8f
jeanhern81 <jeanhern81@gmail.com>
2020-09-02 10:27:45 -0700

    created api and html routes, models folder, running on server but links not working

--- server.js
+++ server.js
@@ -1,22 +1,25 @@
+//packages
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
 
 const app = express();
+
+//port
 const PORT = process.env.PORT || 3000;
 
-
+app.use(morgan("dev"));
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-
-//require("./routes/apiRoutes")(app);
-//require("./routes/htmlRoutes")(app);
+//routes
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, function () {
     console.log(`App running on port ${PORT}!`);
-  
+
 });
