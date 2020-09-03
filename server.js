5034d2125d601f32404afcd5792a02afe626fc63
jeanhern81 <jeanhern81@gmail.com>
2020-09-03 10:21:59 -0700

    trying to fix heroku and atlas connection working in local host but not on heroku

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout-tracker", { useNewUrlParser: true, useUnifiedTopology: true, });
 
-
+/*
 mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/workout-tracker',
     {
@@ -27,7 +28,7 @@ mongoose.connect(
     useFindAndModify: false
     }
 );
-
+*/
 
 
 //routes
