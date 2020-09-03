de60e5dc456fff1144571c8e05d93e1329115638
jeanhern81 <jeanhern81@gmail.com>
2020-09-03 09:40:32 -0700

    trying to fix atlas and heroku connection

--- server.js
+++ server.js
@@ -16,13 +16,12 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
-
 mongoose.connect(
     process.env.MONGODB_URI || "mongodb://localhost/workout-tracker",
     {
         useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useCreateIndex: true,
+        //useUnifiedTopology: true,
+        //useCreateIndex: true,
         useFindAndModify: false
     }
 );
