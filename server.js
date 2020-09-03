fcc24ec710d3a07b27a072d986302210a49e524e
jeanhern81 <jeanhern81@gmail.com>
2020-09-03 09:44:55 -0700

    trying to fix atlas heroku connection

--- server.js
+++ server.js
@@ -16,9 +16,8 @@ app.use(express.json());
 app.use(express.static('public'));
 
 
-mongoose.connect(
-    process.env.MONGODB_URI || "mongodb://localhost/workout-tracker",
-    {
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout-tracker";
+mongoose.connect(MONGODB_URI, {
         useNewUrlParser: true,
         //useUnifiedTopology: true,
         //useCreateIndex: true,
